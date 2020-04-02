ab270fce7e735b77e0702904087ee78e37c2da3a
Pablo Lezcano <47581770+pablolezcano@users.noreply.github.com>
2020-04-02 19:52:50 -0300

    change graph static

--- assets/js/graph-locales.js
+++ assets/js/graph-locales.js
@@ -1,64 +1,3 @@
-///// Graph Bar - Chart 1
-
-google.load('visualization', '1.0', {'packages':['corechart']});
-        
-google.setOnLoadCallback(drawSheetName);
-
- function drawSheetName() {
-
-    var query = new google.visualization.Query(
-    'https://docs.google.com/spreadsheets/d/1LnBXoeEnuGyaNgxqbfx7McvNviiEl52vdXrClxDqH1A/edit#gid=0&range=A1:F9');
-    query.send(handleSampleDataQueryResponse);
-  }
-
-  function handleSampleDataQueryResponse(response) {
-    if (response.isError()) {
-      alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-      return;
-    }
-
-    var data = response.getDataTable();
-    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-    chart.draw(data, {height: '500', width: '100%',});
-  }
-
-
-
-///////// Graph Table - Chart 2
-
-google.charts.load('current', {packages: ['table']});
-
-        
-google.setOnLoadCallback(drawChart1);
-
- function drawChart1() {
-    var queryString = encodeURIComponent('SELECT A:B7');
-
-    var query1 = new google.visualization.Query(
-    'https://docs.google.com/spreadsheets/d/1LnBXoeEnuGyaNgxqbfx7McvNviiEl52vdXrClxDqH1A/edit#gid=0&headers=1&tq=' + queryString);   ///// poner el url del sheets con los permisos en público
-    query1.send(handleSampleDataQueryResponse1);  
-  }
-
-  function resizeHandler () {
-    chart.drawChart1(data, options);
-}
-if (window.addEventListener) {
-    window.addEventListener('resize', resizeHandler, false);
-}
-else if (window.attachEvent) {
-    window.attachEvent('onresize', resizeHandler);
-}
-
-  function handleSampleDataQueryResponse1(response) {
-    if (response.isError()) {
-      alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-      return;
-    }
-
-    var data = response.getDataTable();
-    var chart = new google.visualization.Table(document.getElementById('tabla'));
-    chart.draw(data, {height: '480',width: '100%',});
-}
 
 ///// • Para poner varios gráficos en uno, tenes que enumerar las variables para que no se mezclen 
 /////  entre ellos. Ej.: 
@@ -71,3 +10,70 @@ else if (window.attachEvent) {
 
  ////  https://docs.google.com/spreadsheets/d/1LnBXoeEnuGyaNgxqbfx7McvNviiEl52vdXrClxDqH1A/edit#gid=0&range=A1:F9
 ///// SELECT A1:F9
+
+google.charts.load('current', {'packages':['line']});
+      google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'Day');
+      data.addColumn('number', 'Guardians of the Galaxy');
+      data.addColumn('number', 'The Avengers');
+      data.addColumn('number', 'Transformers: Age of Extinction');
+
+      data.addRows([
+        [1,  37.8, 80.8, 41.8],
+        [2,  30.9, 69.5, 32.4],
+        [3,  25.4,   57, 25.7],
+        [4,  11.7, 18.8, 10.5],
+        [5,  11.9, 17.6, 10.4],
+        [6,   8.8, 13.6,  7.7],
+        [7,   7.6, 12.3,  9.6],
+        [8,  12.3, 29.2, 10.6],
+        [9,  16.9, 42.9, 14.8],
+        [10, 12.8, 30.9, 11.6],
+        [11,  5.3,  7.9,  4.7],
+        [12,  6.6,  8.4,  5.2],
+        [13,  4.8,  6.3,  3.6],
+        [14,  4.2,  6.2,  3.4]
+      ]);
+
+      var options = {
+        chart: {
+          title: 'Box Office Earnings in First Two Weeks of Opening',
+          subtitle: 'in millions of dollars (USD)'
+        },
+        width: '100%',
+        height: 500,
+        axes: {
+          x: {
+            0: {side: 'top'}
+          }
+        }
+      };
+
+      var chart = new google.charts.Line(document.getElementById('line_top_x'));
+
+      chart.draw(data, google.charts.Line.convertOptions(options));
+    }
+
+    google.charts.load('current', {'packages':['table']});
+      google.charts.setOnLoadCallback(drawTable);
+
+      function drawTable() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Name');
+        data.addColumn('number', 'Salary');
+        data.addColumn('boolean', 'Full Time Employee');
+        data.addRows([
+          ['Mike',  {v: 10000, f: '$10,000'}, true],
+          ['Jim',   {v:8000,   f: '$8,000'},  false],
+          ['Alice', {v: 12500, f: '$12,500'}, true],
+          ['Bob',   {v: 7000,  f: '$7,000'},  true]
+        ]);
+
+        var table = new google.visualization.Table(document.getElementById('table_div'));
+
+        table.draw(data, {showRowNumber: true, width: '100%', height: '480'});
+      }
